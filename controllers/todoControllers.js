495be8345fda635802d25b999dca101459238466
Rohit Ramesh <rohitramesh47@gmail.com>
2018-02-03 14:17:37 +0530

    Add delete and add items feature

--- controllers/todoControllers.js
+++ controllers/todoControllers.js
@@ -1,17 +1,24 @@
+var bodyParser = require('body-parser');
+
+var data = [{item: 'get bread'}, {item: 'code'}, {item: 'tell hi'}];
+var urlencodedParser = bodyParser.urlencoded({extended: false});
+
 module.exports = function(app){
 
 app.get('/todo', function(req, res){
-	res.render('todo');
+	res.render('todo', {todos: data});
 });
 
-app.post('/todo', function(req, res){
-
-
+app.post('/todo', urlencodedParser, function(req, res){
+	data.push(req.body);
+	res.json(data);
 });
 
-app.delete('/todo', function(req, res){
-
-
+app.delete('/todo/:item', function(req, res){
+	data = data.filter(function(todo){
+		return todo.item.replace(/ /g, '-') !== req.params.item;
+	});
+	res.json(data);
 });
 
 };
